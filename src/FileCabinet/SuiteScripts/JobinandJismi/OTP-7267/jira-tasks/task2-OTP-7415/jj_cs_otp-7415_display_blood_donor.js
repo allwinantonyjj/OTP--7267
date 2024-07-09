ba33e753fd58d376d4fedbab704efa9e78ebc8dd
allwinantonyjj <allwin.antony@jobinandjismi.com>
2024-07-10 00:27:32 +0530

    otp-7267-ci-added task otp-7415 file

--- src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7267/jira-tasks/task2-OTP-7415/jj_cs_otp-7415_display_blood_donor.js
+++ src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7267/jira-tasks/task2-OTP-7415/jj_cs_otp-7415_display_blood_donor.js
@@ -1,172 +1,81 @@
 /**
- * @NApiVersion 2.x
+ * @NApiVersion 2.1
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
-define(['N/format', 'N/record', 'N/search'],
-/**
- * @param{format} format
- * @param{record} record
- * @param{search} search
- */
-function(format, record, search) {
-    
-    /**
-     * Function to be executed after page is initialized.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-     *
-     * @since 2015.2
-     */
-    function pageInit(scriptContext) {
+/**********************************************************************************
+ * OTP-7415 : Search through the database to find the matching blood donors
+ * 
+ * 
+ * ********************************************************************************
+ * 
+ * ********************
+ * company name
+ * 
+ * Author: Jobin and Jismi IT Services
+ * 
+ * 
+ * Date Created: 05-July-2024
+ * 
+ * Description: This script is for displaying the data in a custom record concurrently based on filters provided by the user in the custom form.
+ * 
+ * 
+ * REVISION HISTORY
+ * 
+ * @version 1.0 company name: 05-July-2024: Created the initial build by JJ0350
+ * 
+ * 
+ * 
+ **************/
+define(['N/currentRecord', 'N/url', 'N/ui/dialog', 'N/log'],
+    function (currentRecord, url, dialog, log) {
+        function fieldChanged(scriptContext) {
+            let bloodDonorRec = scriptContext.currentRecord;
 
-    }
+            if (scriptContext.fieldId === 'custpage_jj_blood' || scriptContext.fieldId === 'custpage_jj_lastdon') {
+                try {
+                    let blood = bloodDonorRec.getValue('custpage_jj_blood');
+                    let lastDonation = bloodDonorRec.getValue('custpage_jj_lastdon');
 
-    /**
-     * Function to be executed when field is changed.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     * @param {string} scriptContext.fieldId - Field name
-     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-     *
-     * @since 2015.2
-     */
-    function fieldChanged(scriptContext) {
+                    log.debug('Field Changed', { blood, lastDonation });
 
-    }
+                    if (lastDonation && !(isDateBeforeThreeMonths(lastDonation))) {
+                        dialog.alert({
+                            title: 'Invalid Date',
+                            message: 'Last donation date must be at least three months before today.'
+                        });
+                        return false;
+                    }
 
-    /**
-     * Function to be executed when field is slaved.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     * @param {string} scriptContext.fieldId - Field name
-     *
-     * @since 2015.2
-     */
-    function postSourcing(scriptContext) {
+                    let suiteletUrl = url.resolveScript({
+                        deploymentId: 'customdeploy_jj_sl_otp7415_jira_task2',
+                        scriptId: 'customscript_jj_sl_otp7415_jira_task2',
+                        params: {
+                            clientBlood: blood,
+                            clientLastDonation: lastDonation
+                        }
+                    });
 
-    }
+                    log.debug('Suitelet URL', suiteletUrl);
 
-    /**
-     * Function to be executed after sublist is inserted, removed, or edited.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     *
-     * @since 2015.2
-     */
-    function sublistChanged(scriptContext) {
+                    if (blood && lastDonation) {
+                        window.location.href = suiteletUrl;
+                    }
+                } catch (error) {
+                    log.error('Error in Client Script', error);
+                }
+            }
+        }
 
-    }
+        function isDateBeforeThreeMonths(selectedDate) {
+            let currentDate = new Date();
+            let threeMonthsAgo = new Date();
+            threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
+            let selected = new Date(selectedDate);
+            return selected < threeMonthsAgo;
+        }
 
-    /**
-     * Function to be executed after line is selected.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     *
-     * @since 2015.2
-     */
-    function lineInit(scriptContext) {
-
-    }
-
-    /**
-     * Validation function to be executed when field is changed.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     * @param {string} scriptContext.fieldId - Field name
-     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-     *
-     * @returns {boolean} Return true if field is valid
-     *
-     * @since 2015.2
-     */
-    function validateField(scriptContext) {
-
-    }
-
-    /**
-     * Validation function to be executed when sublist line is committed.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     *
-     * @returns {boolean} Return true if sublist line is valid
-     *
-     * @since 2015.2
-     */
-    function validateLine(scriptContext) {
-
-    }
-
-    /**
-     * Validation function to be executed when sublist line is inserted.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     *
-     * @returns {boolean} Return true if sublist line is valid
-     *
-     * @since 2015.2
-     */
-    function validateInsert(scriptContext) {
-
-    }
-
-    /**
-     * Validation function to be executed when record is deleted.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @param {string} scriptContext.sublistId - Sublist name
-     *
-     * @returns {boolean} Return true if sublist line is valid
-     *
-     * @since 2015.2
-     */
-    function validateDelete(scriptContext) {
-
-    }
-
-    /**
-     * Validation function to be executed when record is saved.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.currentRecord - Current form record
-     * @returns {boolean} Return true if record is valid
-     *
-     * @since 2015.2
-     */
-    function saveRecord(scriptContext) {
-
-    }
-
-    return {
-        pageInit: pageInit,
-        fieldChanged: fieldChanged,
-        postSourcing: postSourcing,
-        sublistChanged: sublistChanged,
-        lineInit: lineInit,
-        validateField: validateField,
-        validateLine: validateLine,
-        validateInsert: validateInsert,
-        validateDelete: validateDelete,
-        saveRecord: saveRecord
-    };
-    
-});
+        return {
+            fieldChanged: fieldChanged
+        };
+    });
--- src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7267/jira-tasks/task2-OTP-7415/jj_sl_otp-7415_display_blood_donor.js
+++ src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7267/jira-tasks/task2-OTP-7415/jj_sl_otp-7415_display_blood_donor.js
@@ -2,178 +2,164 @@
  * @NApiVersion 2.1
  * @NScriptType Suitelet
  */
+/**********************************************************************************
+ * OTP-7415 : Search through the database to find the matching blood donors
+ * 
+ * 
+ * ********************************************************************************
+ * 
+ * ********************
+ * company name
+ * 
+ * Author: Jobin and Jismi IT Services
+ * 
+ * 
+ * Date Created: 05-July-2024
+ * 
+ * Description: This script is for displaying the data in a custom record concurrently based on filters provided by the user in the custom form.
+ * 
+ * 
+ * REVISION HISTORY
+ * 
+ * @version 1.0 company name: 05-July-2024: Created the initial build by JJ0350
+ * 
+ * 
+ * 
+ **************/
 define(['N/format', 'N/record', 'N/search', 'N/ui/serverWidget'],
-    /**
- * @param{format} format
- * @param{record} record
- * @param{search} search
- */
     (format, record, search, serverWidget) => {
-        /**
-         * Defines the Suitelet script trigger point.
-         * @param {Object} scriptContext
-         * @param {ServerRequest} scriptContext.request - Incoming request
-         * @param {ServerResponse} scriptContext.response - Suitelet response
-         * @since 2015.2
-         */
         const onRequest = (scriptContext) => {
-            if(scriptContext.request.method == 'GET') {
-                let form = serverWidget.createForm({
-                    title: 'Search Eligible Blood Donors',
-                    hideNavBar: true
-                });
-                let filtersField = form.addFieldGroup({
-                    id: 'custpage_jj_filterfield',
-                    label: 'Filters',
-                    // tab: string
-                })
-                let blood = form.addField({
-                    id: 'custpage_jj_blood',
-                    label: 'Blood Group',
-                    type: serverWidget.FieldType.SELECT,
-                    container: 'custpage_jj_filterfield'
-                });
-                // blood.addSelectOption({ 
-                //     value: '', 
-                //     text: '' 
-                // });
-                blood.addSelectOption({
-                    value: 1,
-                    text: 'A+'
-                });
-                blood.addSelectOption({
-                    value: 2,
-                    text: 'A-'
-                });
-                blood.addSelectOption({
-                    value: 3,
-                    text: 'B+'
-                });
-                blood.addSelectOption({
-                    value: 4,
-                    text: 'B-'
-                });
-                blood.addSelectOption({
-                    value: 5,
-                    text: 'O+'
-                });
-                blood.addSelectOption({
-                    value: 6,
-                    text: 'O-'
-                });
-                blood.addSelectOption({
-                    value: 7,
-                    text: 'AB+'
-                });
-                blood.addSelectOption({
-                    value: 8,
-                    text: 'AB-'
-                });
-                blood.addSelectOption({
-                    value: 9,
-                    text: 'Rh null'
-                });
-                let lastDonation = form.addField({
-                    id: 'custpage_jj_lastdon',
-                    label: 'Last donation date',
-                    type: serverWidget.FieldType.DATE,
-                    container: 'custpage_jj_filterfield'
-                });
-                // let sublist = form.addSublist({
-                //     id: 'custpage_jj_sublist',
-                //     label: 'Donor details',
-                //     type: serverWidget.SublistType.LIST
-                // });
-                // sublist.addField({
-                //     id: 'custpage_jj_name',
-                //     label: 'Donor Name',
-                //     type: serverWidget.FieldType.TEXT
-                // });
-                // sublist.addField({
-                //     id: 'custpage_jj_phone',
-                //     label: 'Phone Number',
-                //     type: serverWidget.FieldType.PHONE
-                // });
-                // sublist.addField({
-                //     id: 'custpage_jj_lastdon',
-                //     label: 'Last donation date',
-                //     type: serverWidget.FieldType.DATE
-                // });
-
-                form.addSubmitButton({
-                    label: 'Submit'
-                })
-                scriptContext.response.writePage(form);
-            } else if(scriptContext.request.method == 'POST') {
-                let blood = scriptContext.request.parameters.custpage_jj_blood;
-                let lastDonation = scriptContext.request.parameters.custpage_jj_lastdon;
-                log.debug('group: '+ blood + 'date: ' +lastDonation);
-
-                let searchDonors = search.create({
-                    type: 'customrecord_jj_blooddonorinfo',
-                    filters: [
-                        ['custrecord_jj_donor_blood', 'is', blood],
-                        ['custpage_jj_lastdon', 'onorbefore', lastDonation]
-                    ],
-                    columns: [
-                        'custrecord_jj_donor_fname',
-                        'custrecord_jj_donor_phno',
-                        'custrecord_jj_donor_last_donation_date'
-                    ],
-                    title: 'Blood Donors Details Search JJ',
-                    id: '_jj_displaydonors'
-                });
-
-                let form = serverWidget.createForm({
-                    title: 'Search Eligible Blood Donors',
-                    // hideNavBar: boolean
-                });
-                let sublist = form.addSublist({
-                    id: 'custpage_jj_sublist',
-                    label: 'Donor details',
-                    type: serverWidget.SublistType.LIST
-                });
-                sublist.addField({
-                    id: 'custpage_jj_name',
-                    label: 'Donor Name',
-                    type: serverWidget.FieldType.TEXT
-                });
-                sublist.addField({
-                    id: 'custpage_jj_phone',
-                    label: 'Phone Number',
-                    type: serverWidget.FieldType.PHONE
-                });
-                sublist.addField({
-                    id: 'custpage_jj_lastdon',
-                    label: 'Last donation date',
-                    type: serverWidget.FieldType.DATE
-                });
-
-                let searchResults = searchDonors.run();
-                let i = 0;
-                searchResults.each(function(obj) {
-                    sublist.setSublistValue({
-                        id: 'custpage_jj_name',
-                        line: i,
-                        value: obj.getValue('custrecord_jj_donor_fname')
+            try {
+                if (scriptContext.request.method === 'GET') {
+                    // Creating form
+                    let form = serverWidget.createForm({
+                        title: 'Search Eligible Blood Donors',
+                        hideNavBar: true
                     });
-                    sublist.setSublistValue({
-                        id: 'custpage_jj_phone',
-                        line: i,
-                        value: obj.getValue('custrecord_jj_donor_phno')
+                    // Binding clientScript by its document id in file cabinet
+                    form.clientScriptFileId = 300;
+
+                    let filtersField = form.addFieldGroup({
+                        id: 'custpage_jj_filterfield',
+                        label: 'Filters'
                     });
-                    sublist.setSublistValue({
+
+                    // Adding fields to form
+                    let blood = form.addField({
+                        id: 'custpage_jj_blood',
+                        label: 'Blood Group',
+                        type: serverWidget.FieldType.SELECT,
+                        container: 'custpage_jj_filterfield'
+                    });
+                    blood.addSelectOption({ value: '', text: '' });
+                    blood.addSelectOption({ value: 1, text: 'A+' });
+                    blood.addSelectOption({ value: 2, text: 'A-' });
+                    blood.addSelectOption({ value: 3, text: 'B+' });
+                    blood.addSelectOption({ value: 4, text: 'B-' });
+                    blood.addSelectOption({ value: 5, text: 'O+' });
+                    blood.addSelectOption({ value: 6, text: 'O-' });
+                    blood.addSelectOption({ value: 7, text: 'AB+' });
+                    blood.addSelectOption({ value: 8, text: 'AB-' });
+                    blood.addSelectOption({ value: 9, text: 'Rh null' });
+
+                    let lastDonation = form.addField({
                         id: 'custpage_jj_lastdon',
-                        line: i,
-                        value: obj.getValue('custrecord_jj_donor_last_donation_date')
+                        label: 'Last Donation Date',
+                        type: serverWidget.FieldType.DATE,
+                        container: 'custpage_jj_filterfield'
                     });
-                    
-                    i++;
-                    return true;
-                });
+
+                    let sublist = form.addSublist({
+                        id: 'custpage_jj_sublist',
+                        label: 'Donor Details',
+                        type: serverWidget.SublistType.LIST
+                    });
+                    sublist.addField({
+                        id: 'custpage_jj_name',
+                        label: 'Donor Name',
+                        type: serverWidget.FieldType.TEXT
+                    });
+                    sublist.addField({
+                        id: 'custpage_jj_phone',
+                        label: 'Phone Number',
+                        type: serverWidget.FieldType.PHONE
+                    });
+                    sublist.addField({
+                        id: 'custpage_jj_lastdon',
+                        label: 'Last Donation Date',
+                        type: serverWidget.FieldType.DATE
+                    });
+
+                    // Getting parameters from client
+                    let displayBlood = scriptContext.request.parameters.clientBlood;
+                    let displayLastDonation = scriptContext.request.parameters.clientLastDonation;
+
+                    let date = new Date(displayLastDonation);
+
+                    // Extract the day, month, and year
+                    let day = String(date.getDate()).padStart(2, '0');
+                    let month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+                    let year = date.getFullYear();
+
+                    // Format the date as dd/mm/yyyy
+                    let formattedDate = `${day}/${month}/${year}`;
+
+                    log.debug('Parameters received', { displayBlood, displayLastDonation });
+
+                    if (displayBlood || displayLastDonation) {
+                        let filters = [];
+                        if (displayBlood) {
+                            log.error(formattedDate);
+                            filters.push(['custrecord_jj_donor_blood', 'is', displayBlood], 'AND', ['custrecord_jj_donor_last_donation_date', 'before', formattedDate]);
+                        }
+                        log.error('filter', filters);
+                        // if (formattedDate) {
+                        //     filters.push('AND', ['custrecord_jj_donor_last_donation_date', 'before', formattedDate]);
+                        // }
+                        // log.error('Search Filters', filters);
+                        let searchDonors = search.create({
+                            type: 'customrecord_jj_blooddonorinfo',
+                            filters: filters,
+                            columns: [
+                                'custrecord_jj_donor_fname',
+                                'custrecord_jj_donor_phno',
+                                'custrecord_jj_donor_last_donation_date'
+                            ]
+                        });
+
+                        let searchResults = searchDonors.run();
+                        let i = 0;
+                        searchResults.each(function (obj) {
+                            log.error('Search Result', obj);
+                            sublist.setSublistValue({
+                                id: 'custpage_jj_name',
+                                line: i,
+                                value: obj.getValue('custrecord_jj_donor_fname')
+                            });
+                            sublist.setSublistValue({
+                                id: 'custpage_jj_phone',
+                                line: i,
+                                value: obj.getValue('custrecord_jj_donor_phno')
+                            });
+                            sublist.setSublistValue({
+                                id: 'custpage_jj_lastdon',
+                                line: i,
+                                value: obj.getValue('custrecord_jj_donor_last_donation_date')
+                            });
+                            // log.error(obj.getValue('custrecord_jj_donor_last_donation_date'));
+                            i++;
+                            return true;
+                        });
+                    }
+
+                    // Write form
+                    scriptContext.response.writePage(form);
+                }
+            } catch (error) {
+                log.error('Error in Suitelet', error);
             }
         }
-
-        return {onRequest}
-
+        return { onRequest }
     });
+
+
